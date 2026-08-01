bf0281a0015c1f33b3a923eb1ec1692222335040
jporrasr97 <jporrasr@miumg.edu.gt>
2026-07-31 21:09:31 -0600

    udpate

--- src/routes.js
+++ src/routes.js
@@ -29,13 +29,15 @@ import {
     processEditOrganizationForm   
 } from './controllers/organizations.js';
 
-// Consolidate all user imports into this single block
+
 import { 
     showUserRegistrationForm, 
     processUserRegistrationForm,
     showLoginForm,
     processLoginForm,
-    processLogout
+    processLogout,
+    requireLogin,
+    showDashboard
 } from './controllers/users.js';
 
 const router = express.Router();
@@ -68,5 +70,8 @@ router.post('/register', processUserRegistrationForm);
 router.get('/login', showLoginForm);
 router.post('/login', processLoginForm);
 router.get('/logout', processLogout);
+
+
+router.get('/dashboard', requireLogin, showDashboard);
 
 export default router;
